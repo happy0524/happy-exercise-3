6af6064fcc21d075e5f369494f3781aa521f7996
happy0524 <jhcha524@gmail.com>
2025-02-25 23:34:29 +0900

    Practice complex loop and array problems
    Brainstorming process

---
+++ loop&array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	int key[] = getchar();
+	int a;
+
+	while (1) {
+
+		if (getchar() == 1) {
+			break;
+		}
+		else if (getchar() == 'E') {
+			break;
+		};
+
+	}
+
+	return 0;
+}
